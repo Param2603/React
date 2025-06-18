93caaf4f32eba1951f6e1e3abd47d831bde2d58a
Param2603 <ps26032005@gmail.com>
2025-06-18 00:00:48 -0700

    tailwindcss

--- day4tailwind/my-react-app/src/components/Navbar.jsx
+++ day4tailwind/my-react-app/src/components/Navbar.jsx
@@ -2,15 +2,16 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <nav>
-        <p>CryptoM5</p>
-        <ul>
+    <nav className='flex justify-between cursor-pointer h-[70px]  items-center px-15 '>
+        <p className=''>CryptoM5</p>
+        <ul className='flex gap-5'>
             <li>Home</li>
             <li>About</li>
             <li>Features</li>
             <li>Contacts</li>
+             <button className='bg-purple-400 cursor-pointer rounded-2xl'>Get Started</button>
         </ul>
-        <button>Get Started</button>
+       
     </nav>
   )
 }
