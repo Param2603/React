9fa4d06489ac58083f0639892ab039e95ed0a33a
Param2603 <ps26032005@gmail.com>
2025-06-19 12:49:51 -0700

    tailwindcss

--- day4tailwind/my-react-app/src/components/Navbar.jsx
+++ day4tailwind/my-react-app/src/components/Navbar.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <nav className='flex justify-between cursor-pointer h-[70px]  items-center px-15 '>
+    <nav className='flex justify-between cursor-pointer h-[70px]  items-center px-15'>
         <p className='text-[#232323] text-[2rem]  font-sans  font-bold'>CryptoM5</p>
         <ul className='flex gap-10'>
             <li className='text-[#232323] text-[1rem] font-sans  font-medium'>Home</li>
             <li className='text-[#232323] text-[1rem] font-sans  font-medium'>About</li>
             <li className='text-[#232323] text-[1rem] font-sans  font-medium'>Features</li>
             <li className='text-[#232323] text-[1rem]  font-sans font-medium'>Contacts</li>
-            <button className='bg-[#8364e2] text-[#ffffff]  cursor-pointer rounded-[2rem] w-auto' >Get Started</button>
+            <button className='bg-[#8364e2] text-[#ffffff]  cursor-pointer rounded-[2rem] w-[150px] ' >Get Started</button>
         </ul>
        
     </nav>
--- day4tailwind/my-react-app/src/components/Section.jsx
+++ day4tailwind/my-react-app/src/components/Section.jsx
@@ -5,13 +5,13 @@ const Section = () => {
     <section className='flex justify-between  p-15'>
       <div className=''>
          <p className='text-[#8364E2] p-1 font-bold font-sans text-[20px]' >CRYPTO PLATFORM</p>
-        <p className='p-1 text-[#0D0C22]  text-[59px] font-sans font-bold'>Crypto Trade <br/> Agency</p>
+        <p className='p-1 text-[#0D0C22]  text-[59px] font-sans font-bold'>Crypto Trade <br/>Agency</p>
         <p className='p-1 text-[#727272] text-[20px] font-sans'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed <br/> eiusmod tempor incididunt labore dolore magna aliqua.</p> <br />
-        <button className='bg-[#8364e2] text-[#ffffff]  cursor-pointer rounded-[2rem] w-auto' >Get Started</button>
+        <button className='bg-[#8364e2] text-[#ffffff]  cursor-pointer rounded-[2rem] w-[150px]' >Get Started</button>
       </div>
 
       <div className='flex'>
-         <img className='h-[400px] w-[500px]  rounded-[2rem]' src="https://mobirise.com/extensions/cryptom5/trade-agency/assets/images/features4.jpg" alt="" />
+         <img className='h-[400px] w-[550px]  rounded-[2rem]' src="https://mobirise.com/extensions/cryptom5/trade-agency/assets/images/features4.jpg" alt="" />
       </div>
     </section> 
   )
