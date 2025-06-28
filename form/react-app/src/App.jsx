a7b7bb7b241d9b320c1c6b8b0b1a30f292dcab17
Parth99001 <parthgandhi350@gmail.com>
2025-06-28 16:10:44 +0530

    form

--- form/react-app/src/App.jsx
+++ form/react-app/src/App.jsx
@@ -1,37 +1,65 @@
 import React, { useState } from 'react'
 
 const App = () => {
-  const [data,setData] = useState({name:"" , email:"" ,password:""})
+  const [data, setData] = useState({ name: "", email: "", password: "", confirmpassword: "" })
+  const [submitData, setSubmitData] = useState(null)
+  const [error, setError] = useState({})
 
-  function handleSubmit (e){
+  function handleSubmit(e) {
     e.preventDefault()
-    
-    
+    if (validation()) {
+      setSubmitData(data)
+      setData({ name: "", email: "", password: "", confirmpassword: "" })
+
+    }
+  }
+
+  function validation() {
+    let obj = {}
+    let val = true
+
+    if (!data.name.trim()) {
+      val = false
+      obj.name = "name is Reqired"
+    }
+    if (!data.email.trim()) {
+      val = false
+      obj.email = "email is Reqired"
+    }
+    if (!data.password.trim()) {
+      val = false
+      obj.password = "password is Reqired"
+    }
+
+    setError(obj)
+    return val
   }
 
   return (
-    <form onSubmit={handleSubmit} action="">
-      <div>
+    <form className='bg-[#f5f5f5] text-black border-2 border- px-2 py-4' onSubmit={handleSubmit} action="">
+      <div className='mb-5'>
         <label htmlFor="">Name:</label>
-        <input onChange={(e)=>setData({... data,name:e.target.value
+        <input className='border-1 rounded-[8px] ml-2' onChange={(e) => setData({
+          ...data, name: e.target.value
         })} value={data.name} type="text" />
+        {error.name && <p>{error.name}</p>}
       </div>
-      <div>
+      <div className='mb-5'>
         <label htmlFor="">Email:</label>
-        <input onChange={(e)=>setData({... data,email:e.target.value
+        <input className='border-1 rounded-[8px] ml-2' onChange={(e) => setData({
+          ...data, email: e.target.value
         })} value={data.email} type="text" />
+        {error.email && <p>{error.email}</p>}
       </div>
-      <div>
+      <div className='mb-5'>
         <label htmlFor="">Password:</label>
-        <input onChange={(e)=>setData({... data,password:e.target.value
-        })} value={data.password} type="number" />
+        <input className='border-1 rounded-[8px] ml-2' onChange={(e) => setData({
+          ...data, password: e.target.value
+        })} value={data.password} type="password" />
+        {error.password && <p>{error.password}</p>}
       </div>
-      <div>
-        <label htmlFor="">Confirm Password:</label>
-        <input onChange={(e)=>setData({... data,confirmpassword:e.target.value
-        })} value={data.confirmpassword} type="number" />
-      </div>
-      <button>Submit</button>
+
+      <button className='cursor-pointer bg-amber-300 hover:bg-amber-200 hover:text-blue-400 px-2 py-2 rounded-[15px] text-red-500'>Submit</button>
     </form>
   )
 }
