562091f680151de1fc14c6318989014e07a9fe4e
Parth99001 <parthgandhi350@gmail.com>
2025-07-11 16:33:00 +0530

    proj-4

--- Project/proj-4/react-app/src/App.jsx
+++ Project/proj-4/react-app/src/App.jsx
@@ -2,15 +2,13 @@ import React from 'react'
 import { Route, Routes } from 'react-router'
 import Cart from './Components/Cart'
 import Productpage from './Components/Productpage'
-import Home from './Components/Home'
 
 const App = () => {
   return (
     <div>
       <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/cart' element={<Cart/>}></Route>
-        <Route path='/productpage' element={<Productpage/>}></Route>
+        <Route path='/' element={<Cart/>}/>
+        <Route path='/productpage' element={<Productpage/>}/>
       </Routes>
     </div>
   )
--- Project/proj-4/react-app/src/Components/Cart.jsx
+++ Project/proj-4/react-app/src/Components/Cart.jsx
@@ -1,13 +1,16 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
-import { Link } from 'react-router'
+
 
 const Cart = () => {
-  const [data, setData] = useState({name:"",title:"",category:""})
+  const [data, setData] = useState([])
+  const [inputData , setInputData] = useState({name:"",title:"",category:""})
 
-  async function handleSubmit(){
-    let res = await axios.post("http://localhost:3000/Products")
-    setData(res.data)
+  async function handleSubmit(e){
+    e.preventDefault()
+    let res = await axios.post("http://localhost:3000/Products" , inputData)
+    setData([res.data])
+    console.log([res.data])
   }
 
     async function getData(){
@@ -15,10 +18,6 @@ const Cart = () => {
     setData(res.data)
   }
 
-  function handleSubmit (){
-    e.preventDefault()
-    console.log(data)
-  }
 
   useEffect(() => {
     getData()
@@ -26,22 +25,28 @@ const Cart = () => {
   return (
     <div>   
       <form onSubmit={handleSubmit} action="">
-        <input value={data.name} onChange={(e) => setData({...data, name: e.target.value})} type="text" placeholder='Name' /> 
-      <input value={data.title} onChange={(e) => setData({...data, title: e.target.value})} type="text" placeholder='Title' /> 
-      <input value={data.category} onChange={(e) => setData({...data, category: e.target.value})} type="text" placeholder='Category' />
+        <input value={inputData.name} onChange={(e) => setInputData({...inputData, name: e.target.value})} type="text" placeholder='Name' /> 
+      <input value={inputData.title} onChange={(e) => setInputData({...inputData, title: e.target.value})} type="text" placeholder='Title' /> 
+      <input value={inputData.category} onChange={(e) => setInputData({...inputData, category: e.target.value})} type="text" placeholder='Category' />
 
       <div>
           <button>Add Cart</button> 
       </div>
 
 
- {setData && <div>
-          <p>{setData.name}</p>
-          <p>{setData.title}</p>
-          <p>{setData.category}</p>
-          </div>}
-      
+        
         </form> 
+
+        {data && data.map((item)=>{
+              return(
+                <div key={item.id}>
+                  <p>title :- {item.title}</p>
+                  <p>Name :- {item.name}</p>
+                </div>
+              )
+        })
+
+        }
 
        
     </div>
