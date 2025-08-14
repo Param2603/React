7fd53dbdfc87e9eed878c8bafce07ba56dfc985a
Param2603 <ps26032005@gmail.com>
2025-08-15 00:33:36 +0530

    redux

---
+++ Project/react-proj-8/react-app/src/components/TodoList.jsx
@@ -0,0 +1,105 @@
+import React, { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux';
+import { addTask, deleteTask, editTask, searchTask, toggleComplete } from '../redux/todo/todoSlice';
+import { nanoid } from 'nanoid';
+
+const TodoList = () => {
+    const [data,setData] = useState("")
+    const dispatch = useDispatch()
+    const {todo, filter} = useSelector((state) => state.todo)
+    // console.log(todo)
+    const [editText,setEditText] = useState("")
+    const [editId,setEditId] = useState(null)
+
+    
+
+
+    function handleSubmit(e){
+        e.preventDefault();
+        if(data.trim() === '') return
+        dispatch(addTask({
+            id: nanoid(),
+            text: data.trim(),
+            completed: false,
+        }))
+        alert('Task Added Successfully')
+        setData("")
+    }
+
+    function handleEdit (id,text){
+        setEditId(id)
+        setEditText(text)
+    }
+
+    function handleUpdate (id){
+        if(editText.trim()){
+            dispatch(editTask({id,nextText: editText.trim()}))
+            setEditId(null)
+            setData("")
+            setEditText("")
+            alert('Task Updated Successfully')
+        }
+    }
+
+  return (
+    <div className='max-w-xl mx-auto p-4'>
+        <p className='text-[25px] font-bold mb-4 pt-15'>Todo List</p>
+
+        <form onSubmit={handleSubmit} className='flex items-center gap-2 mb-4'>
+
+            <input type="text" 
+            value={data}
+            onChange={(e) => setData(e.target.value)}
+            placeholder='Enter New Task'  
+            className='p-2 border rounded-[5px] flex-1' />
+
+            <button type='submit'       className='bg-blue-500 text-white px-6 py-2 rounded-[5px] hover:bg-blue-600 cursor-pointer'>Add Task</button>
+        </form>
+
+        <div>
+          <input type="text" value={filter.search}
+          onChange={(e) => dispatch(searchTask(e.target.value))}
+          className='w-full p-2 border rounded mb-4'
+          placeholder='Search...'/>  
+        </div>
+
+        <div>
+            <ul className='space-y-2'>
+                {todo.length === 0 && <p>No Tasks Found</p>}
+                {todo.map(data => (<li key={data.id} className='flex items-center justify-between p-3 border rounded'>
+                    <div className='flex items-center gap-2 '>
+                        <input type="checkbox" checked={data.completed} onChange={(e) => dispatch(toggleComplete(data.id))} 
+                        className='border rounded px-2'/>
+                        {
+                           editId === data.id ? (
+                            <input type="text" value={editText} onChange={(e) => setEditText(e.target.value)}
+                            className='border rounded px-2 ' />
+                           ) : (
+                            <span className={data.completed ? 'line-through text-gray-500':''}>{data.text}</span>
+                           )
+                        }
+                    </div>
+
+                    <div>
+                        {
+                            editId === data.id ? (
+                                <button 
+                                onClick={() => handleUpdate(data.id )}
+                                className='text-gray-600 hover:underline cursor-pointer'>Update</button>
+                            ) : (
+                                <button 
+                                onClick={() => handleEdit(data.id,data.text)} className='text-blue-600 hover:underline cursor-pointer'>Edit</button>                  
+                            )
+                        }
+
+                        <button onClick={() => dispatch(deleteTask(data.id))} className='text-red-500 hover:underline cursor-pointer pl-2'>Delete</button>
+
+                    </div>
+                </li>))}
+            </ul>
+        </div>
+    </div>
+  )
+}
+
+export default TodoList
--- Project/react-proj-8/react-app/src/redux/todo/todoSlice.js
+++ Project/react-proj-8/react-app/src/redux/todo/todoSlice.js
@@ -23,7 +23,39 @@ const todoSlice = createSlice({
         }
     },
     reducers: {
-        
-    }
+        addTask: (state, action) => {
+            state.todo.push(action.payload);
+            saveDataToLocalStorage(state.todo)
+        },
 
+        deleteTask: (state, action) => {
+            state.todo = state.todo.filter(todo => todo.id !== action.payload);
+            saveDataToLocalStorage(state.todo)
+        },
+
+        toggleComplete: (state,action) => {
+            const todoData = state.todo.find(t => t.id === action.payload);
+            if(todoData) todoData.completed = !todoData.completed;
+            saveDataToLocalStorage(state.todo)
+        },
+
+        editTask: (state,action) => {
+            const {id, nextText} = action.payload;
+            const todoData = state.todo.find(t => t.id === id);
+            if(todoData) todoData.text = nextText;
+            saveDataToLocalStorage(state.todo)
+        },
+
+        filterTask: (state,action) => {
+            state.filter.status = action.payload
+        },
+
+        searchTask: (state,action) => {
+            state.filter.search = action.payload
+        }
+    }
 })
+
+export const { addTask,deleteTask,editTask,filterTask,searchTask,toggleComplete } = todoSlice.actions
+
+export default todoSlice.reducer
